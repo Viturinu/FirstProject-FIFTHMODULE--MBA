f63e50be176b0c0587e419a40aff08e1f85e4b9f
Victor Oliveira Almeida <victor.almeida.ti@gmail.com>
2025-07-08 18:19:47 -0300

    add: icons and names

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,8 +1,10 @@
+//import "./style.css" //não foi preciso pois eu já adicionei no index.html
+
 export function App(): React.JSX.Element {
 
   return (
     <>
-      <text className="text-3xl">Teste</text>
+      <text className="text-3xl font-bold" style={{ fontFamily: "Inter" }}>Primeiro App Desktop</text>
     </>
   )
 }
